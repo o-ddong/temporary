6aaaa63cfdc9a32a1a0c9a5d9d7ca78fe18bcb4f
odh0112 <odh0112@naver.com>
2021-02-23 22:03:41 +0900

    gnl_code

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "fcntl.h"
+# include "unistd.h"
+# include "stdlib.h"
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+int		get_next_line(int fd, char **line);
+int		line_split(char **line, char **backup);
+int		return_value(int read_size, char **line, char *backup);
+int		ft_strlen(const char *str);
+char	*ft_strdup(const char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, int len);
+
+#endif
---
+++ get_next_line_bonus.c
@@ -0,0 +1,55 @@
+#include "get_next_line_bonus.h"
+
+int                 word_split(char **line, char *backup)
+{
+    char            *tmp1;
+
+    if (tmp1 = ft_strchr(backup, '\n'))
+    {
+        *line = ft_substr(backup, 0, tmp1 - backup);
+        tmp1 = ft_strdup(tmp1 + 1);
+        free(backup);
+        backup = tmp1;
+    }
+    else
+    {
+        *line = ft_strdup(backup);
+        free(backup);
+        backup = NULL;
+        return (0);
+    }
+    return (1);
+}
+
+int                 return_value(int read_size, char **line, char *backup)
+{
+    if (read_size < 0)
+        return (-1);
+    else if (read_size == 0 && !*backup)
+    {
+        free(backup);
+        *line = ft_strdup("");
+        return (0);
+    }
+    else
+        return (word_split(line, backup));
+}
+
+int	                get_next_line(int fd, char **line)
+{
+    static char     *backup[1024];
+    char            buf[BUFFER_SIZE + 1];
+    int             read_size;
+
+    if (fd < 0 || BUFFER_SIZE < 1 || line == NULL)
+        return (-1);
+    if (backup[fd] == NULL)
+        backup[fd] = ft_strdup("");
+    while (!ft_strchr(backup[fd], 'n') 
+            && (read_size = read(fd, buf, BUFFER_SIZE)) > 0)
+    {
+        buf[read_size] = 0;
+        backup[fd] = ft_strjoin(backup[fd], buf);
+    }
+    return (return_value(read_size, line, backup[fd]));
+}
